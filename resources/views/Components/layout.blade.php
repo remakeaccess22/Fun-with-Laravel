09ae31a71dc7ceb83c111869043ec8456a2137a1
remakeaccess22 <alayacyacjude@gmail.com>
2024-08-02 22:57:46 +0800

    Hover Testing

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -18,7 +18,7 @@
           <div class="hidden md:block">
             <div class="ml-10 flex items-baseline space-x-4">
               <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-              <a href="/" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Home</a>
+              <a href="/" class="{{ true ? 'rounded-md bg-gray-900' : 'text-gray hover:bg-gray-700 hover:text-white' }} px-3 py-2 text-sm font-medium text-white" aria-current="page">Home</a>
               <a href="/about" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
               <a href="/contact" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contacts</a>
             </div>
