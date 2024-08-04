0ee390baa83f55d0934df45d84cd58cc650cc2d2
remakeaccess22 <alayacyacjude@gmail.com>
2024-08-04 23:31:53 +0800

    On going process on layout and nav-link

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -18,9 +18,9 @@
           <div class="hidden md:block">
             <div class="ml-10 flex items-baseline space-x-4">
               <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-              <a href="/" class="{{ true ? 'rounded-md bg-gray-900' : 'text-gray hover:bg-gray-700 hover:text-white' }} px-3 py-2 text-sm font-medium text-white" aria-current="page">Home</a>
-              <a href="/about" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
-              <a href="/contact" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contacts</a>
+              <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
+              <x-nav-link href="/about" :active="request()->is('/about')">About</x-nav-link>
+              <x-nav-link href="/contact" :active="request()->is('/contact')">Contacts</x-nav-link>
             </div>
           </div>
         </div>
