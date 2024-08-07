cc1cf38df49d6c2ee55c7839257a105adc426d41
remakeaccess22 <alayacyacjude@gmail.com>
2024-08-07 23:01:37 +0800

    updated migration and models

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -1,6 +1,13 @@
 <x-layout>
     <x-slot:heading>
-        Jobs Page
+        Job Listings
     </x-slot:heading>
-    <h1>This is Jobs Page</h1>
+    <ul>
+        @foreach ($jobs as $job)
+            <li>
+                <a href="/jobs/{{ $job['id'] }}" class="text-blue-500 hover:underline"><strong>{{ $job['title'] }}
+                    </strong> Pays {{ $job['salary'] }} per year</a>
+            </li>
+        @endforeach
+    </ul>
 </x-layout>
