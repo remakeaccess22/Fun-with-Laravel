56aad399a2c96160ca9a967bc11d5db9543e7970
remakeaccess22 <alayacyacjude@gmail.com>
2024-08-10 23:30:14 +0800

    Two Eloquent relationships test

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -10,4 +10,7 @@ class Job extends Model {
     protected $table ='job_listings';
     protected $fillable =['title', 'salary'];
 
+    public function employer() {
+        return $this->belongsTo(Employer::class);
+    }
 }
