006ef0a6b919bc8840207c9f84e6313ab851d2a8
remakeaccess22 <alayacyacjude@gmail.com>
2024-08-15 11:53:49 +0800

    Pending Update and Delete Routes

--- routes/web.php
+++ routes/web.php
@@ -6,24 +6,26 @@ use App\Models\Job;
 Route::get('/', function () {
     return view('home');
 });
-
+// Display All Jobs
 Route::get('/jobs', function () {
     $jobs = Job::with('employer')->latest()->cursorPaginate(3);
     return view('jobs.index', [
         'jobs' => $jobs
     ]);
 });
-
+// Create a Job
 Route::get('/jobs/create', function () {
     return view('jobs.create');
 });
 
+// Show a Job
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
     return view('jobs.show', [
         'job' => $job]);
 });
 
+// Store a Job
 Route::post('/jobs', function () {
     request()->validate([
         'title' => ['required', 'min:3'],
@@ -38,6 +40,28 @@ Route::post('/jobs', function () {
 
     return redirect('/jobs');
 });
+// Edit a Job
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::find($id);
+    return view('jobs.edit', [
+        'job' => $job]);
+});
+
+// Update a Job
+Route::patch('/jobs/{id}', function ($id) {
+    //validate
+    //authorize
+    //update
+    //and persist
+    //redirect to the job page
+});
+
+// Destroy a Job
+Route::delete('/jobs/{id}', function ($id) {
+
+});
+
+
 
 Route::get('/contact', function () {
     return view('contact');
