3f0a68df98a2e6c4e45ecb81fd52a8917319c8db
remakeaccess22 <alayacyacjude@gmail.com>
2024-08-18 22:19:02 +0800

    Components

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,13 @@
 
 use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisteredUserController;
 
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
 Route::resource('jobs', JobController::class);
+
+//Auth
+Route::get('/register', [RegisteredUserController::class, 'create'])
+    ->middleware('guest')
+    ->name('register');
